a78e4b107330dd47cc124bda630200500f709fa6
Nizar Taoussi <nizar.taoussi@tython.org>
2026-05-07 09:53:04 +0100

    fix: add server migration, merge conflict, fix startup fake

---
+++ nexusplan-backend/auth_service/users/migrations/0003_merge_messaging_migrations.py
@@ -0,0 +1,12 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('users', '0002_add_messaging_models'),
+        ('users', '0002_user_is_online_user_last_seen_directmessage_and_more'),
+    ]
+
+    operations = [
+    ]
