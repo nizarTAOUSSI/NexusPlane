205505f7e1b07a25f557c7c777cef0278db608df
Nizar Taoussi <taoussinizae7@gmail.com>
2026-05-16 22:38:12 +0100

    fix: add 'auth_service' to ALLOWED_HOSTS and import json in views

--- nexusplan-backend/auth_service/auth_service/settings.py
+++ nexusplan-backend/auth_service/auth_service/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = os.environ.get(
 
 DEBUG = os.environ.get("DEBUG", "True") == "True"
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "*").split(",") + ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "*").split(",") + ["localhost", "127.0.0.1", "auth_service"]
 GOOGLE_CLIENT_ID = os.environ.get("GOOGLE_CLIENT_ID", "")
 
 # ---------------------------------------------------------------------------
